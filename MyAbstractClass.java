cb9359f01df1b7e47ee4c59ad9d889086631d212
Harish S <96660482+harishsadhas08@users.noreply.github.com>
2022-02-04 15:08:27 +0530

    Add files via upload

---
+++ MyAbstractClass.java
@@ -0,0 +1,25 @@
+package abstractproject;
+
+public class MyAbstractClass {
+	void show();
+
+	}
+
+	public class MyClass extends MyAbstractClass{
+
+	    void show(){
+
+	               System.out.println("Show");
+
+	    }
+
+	               public static void main(String[] args) {
+
+	                              MyClass obj=new MyClass();
+
+	                              obj.show();
+
+	 }
+
+	}
+
